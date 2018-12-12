b3eca912b0272a06aa5b5ff0a34ff7bf4285af6c
John Godsey <johngodsey@gmail.com>
2018-12-12 12:17:28 -0500

    Working on HTTP implementation.

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/url"
 	"strconv"
 	"strings"
 	"time"
@@ -149,7 +151,11 @@ func thread(key string, source string) {
 	})
 
 	vm.Set("http", map[string]interface{}{
-		"Get": httpGet,
+		"Get":      httpGet,
+		"Post":     httpPost,
+		"PostForm": httpPostForm,
+		"Put":      httpPut,
+		"Delete":   httpDelete,
 	})
 	//Get whole script in memory.
 	_, err := vm.Run(source)
@@ -239,5 +245,96 @@ func httpGet(url string) map[string]interface{} {
 		return map[string]interface{}{"error": err}
 	}
 
-	return map[string]interface{}{"body": string(body), "status": resp.StatusCode}
+	return map[string]interface{}{"body": string(body), "status": resp.StatusCode, "headers": resp.Header}
+}
+
+func httpHead(url string) map[string]interface{} {
+	resp, err := http.Head(url)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+
+	return map[string]interface{}{"headers": resp.Header, "status": resp.StatusCode}
+}
+
+func httpPost(url string, body string) map[string]interface{} {
+	client := http.Client{}
+	request, err := http.NewRequest(http.MethodPost, url, bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+
+	return map[string]interface{}{"body": string(respBody), "status": resp.StatusCode, "headers": resp.Header}
+
+}
+
+func httpPostForm(urlString string, formData map[string]interface{}) map[string]interface{} {
+	values := mapToValues(formData)
+	resp, err := http.PostForm(urlString, values)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+	return map[string]interface{}{"body": string(body), "status": resp.StatusCode, "headers": resp.Header}
+}
+
+func httpPut(url string, body string) map[string]interface{} {
+	client := http.Client{}
+	request, err := http.NewRequest(http.MethodPut, url, bytes.NewBuffer([]byte(body)))
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+
+	return map[string]interface{}{"body": string(respBody), "status": resp.StatusCode, "headers": resp.Header}
+
+}
+
+func httpDelete(url string) map[string]interface{} {
+	client := http.Client{}
+	request, err := http.NewRequest(http.MethodDelete, url, nil)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+
+	resp, err := client.Do(request)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+	respBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return map[string]interface{}{"error": err}
+	}
+
+	return map[string]interface{}{"body": string(respBody), "status": resp.StatusCode, "headers": resp.Header}
+
+}
+
+func mapToValues(m map[string]interface{}) (values url.Values) {
+	values = url.Values{}
+	for key, value := range m {
+		values.Set(key, fmt.Sprint(value))
+	}
+	return
 }
